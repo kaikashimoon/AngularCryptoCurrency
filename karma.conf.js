11ef61f28e9b08785f0c937ee46936a0d53bc4bf
kai <leonc4667@gmail.com>
2022-09-12 16:23:11 -0400

    Crypto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/coingecko'),
+      dir: require('path').join(__dirname, './coverage/angular-coingecko-api'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+interface Coin{
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'coingecko';
+export class AppComponent implements OnInit {
+
+  coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume'
+  ];
+  searchFilterCoin = '';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  searchCoin() {
+    this.filteredCoins = this.coins.filter(coin => coin.name.toLocaleLowerCase().includes(this.searchFilterCoin.toLocaleLowerCase()) || coin.symbol.toLocaleLowerCase().includes(this.searchFilterCoin.toLocaleLowerCase()))
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .subscribe(res => {console.log(res);
+      this.coins = res
+      this.filteredCoins = res
+    }, err => {console.log(err)});
+
+  }
 }
